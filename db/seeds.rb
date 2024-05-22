68d05a977cb95d27be77ad0ef50ba3cfd87aeada
Julien Paris <parisjulien87@gmail.com>
2024-05-22 16:32:23 +0200

    exo 2 22

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,22 @@
 #
 # Example:
 #
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#   ['Action', 'Comedy', 'Drama', 'Horror'].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+dishoom = { name: 'Dishoom', address: '7 Boundary St, London E2 7JE', phone_number: '0148776534', category: 'chinese'}
+pizza_east = { name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', phone_number: '0148786534', category:'italian'}
+dishoom2 = { name: 'Dishoom2', address: '7 Boundary St, London E2 7JE', phone_number: '0148773534', category: 'japanese'}
+pizza_east2 = { name: 'Pizza East2', address: '56A Shoreditch High St, London E1 6PQ', phone_number: '0142776534', category:'chinese'}
+pizza_east3 = { name: 'Pizza East3', address: '56A Shoreditch High St, London E1 6PQ', phone_number: '0142876534', category:'italian'}
+
+
+[dishoom, pizza_east, dishoom2, pizza_east2, pizza_east3].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
